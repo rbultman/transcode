4a4f5b7d1851453d022dd675e1cc246f4a58a915
Rob Bultman <rob.bultman@gmail.com>
2013-12-30 20:07:24 -0500

    First working version.

--- transcode.py
+++ transcode.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from subprocess import call
 
 # avconv -i srcfile -b 320k -c libvorbis destfile
 
@@ -16,6 +17,10 @@ def handleFiles(dest, src):
 		target = os.path.join(os.path.dirname(dest), basename + ".ogg")
 		if not os.path.exists(target):
 			print("\tDestination: " + target)
+			rtn = call(["avconv", "-i", src, "-b", "320k", "-c", "libvorbis", target])
+			if rtn != 0:
+				print("ERROR: avconv returned non-zero: " + str(rtn))
+				sys.exit(0)
 		else:
 			print("\tDestination exists, doing nothing.")
 	else:
@@ -28,6 +33,8 @@ def handleFiles(dest, src):
 def transDirs(destDir, srcDir):
 	print(os.path.abspath(destDir) + " <= " + os.path.abspath(srcDir))
 	listDir = os.listdir(srcDir)
+	if not os.path.exists(destDir):
+		os.mkdir(destDir)
 	for f in listDir:
 		if os.path.isfile(srcDir + os.sep + f):
 			handleFiles(destDir + os.sep + f, srcDir + os.sep + f)
